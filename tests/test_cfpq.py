38355c3d8dace83e919398d7fada028bc4a8f794
AvvALlV <cghjhfdshj@gmail.com>
2023-04-22 22:06:12 +0300

    task9 done

--- tests/test_cfpq.py
+++ tests/test_cfpq.py
@@ -1,4 +1,4 @@
-from project.hellings import *
+from project.cfpq import *
 from project.graph_utils import create_labeled_two_cycles_graph
 
 
@@ -37,10 +37,45 @@ B -> b
     }
 
 
+def test_matrix():
+    # из конспекта:
+    cfg = CFG.from_text(
+        """
+S -> A B
+S -> A S1
+S1 -> S B
+A -> a
+B -> b
+"""
+    )
+
+    graph = create_labeled_two_cycles_graph(2, 1)
+    result = matrix_mult(cfg, graph)
+    assert result == {
+        (0, Variable("A"), 1),
+        (1, Variable("A"), 2),
+        (2, Variable("A"), 0),
+        (0, Variable("B"), 3),
+        (3, Variable("B"), 0),
+        (2, Variable("S"), 3),
+        (2, Variable("S1"), 0),
+        (1, Variable("S"), 0),
+        (1, Variable("S1"), 3),
+        (0, Variable("S"), 3),
+        (0, Variable("S1"), 0),
+        (2, Variable("S"), 0),
+        (2, Variable("S1"), 3),
+        (1, Variable("S"), 3),
+        (1, Variable("S1"), 0),
+        (0, Variable("S"), 0),
+        (0, Variable("S1"), 3),
+    }
+
+
 def test_hellings_from_text():
     # из конспекта:
     graph = create_labeled_two_cycles_graph(2, 1)
-    result = helling_from_text(
+    result = algo_from_text(
         """
 S -> A B
 S -> A S1
@@ -71,7 +106,7 @@ B -> b
     }
 
 
-def test_cfpq():
+def test_cfpq_hellings():
     cfg = CFG.from_text(
         """
 S -> A B
@@ -83,6 +118,21 @@ B -> b
     )
     graph = create_labeled_two_cycles_graph(2, 1)
     result = cfpq(cfg, graph)
+    assert result == {0: {0, 3}, 1: {0, 3}, 2: {0, 3}, 3: set()}
+
+
+def test_cfpq_matrix():
+    cfg = CFG.from_text(
+        """
+S -> A B
+S -> A S1
+S1 -> S B
+A -> a
+B -> b
+"""
+    )
+    graph = create_labeled_two_cycles_graph(2, 1)
+    result = cfpq(cfg, graph, algo=cfpqAlgo.MATRIX)
     assert result == {0: {0, 3}, 1: {0, 3}, 2: {0, 3}, 3: set()}
 
 
